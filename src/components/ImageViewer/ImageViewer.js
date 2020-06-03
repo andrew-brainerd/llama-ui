b16fe59f241190d48783c6351d6149aa1c96a816
Andrew Brainerd <drwb333@gmail.com>
2020-06-03 19:29:46 -0400

    Add G rating

--- src/components/ImageViewer/ImageViewer.js
+++ src/components/ImageViewer/ImageViewer.js
@@ -1,11 +1,16 @@
 import React, { useEffect } from 'react';
 import { bool, shape, string, func } from 'prop-types';
+import useInterval from '../../hooks/useInterval';
 import styles from './ImageViewer.module.scss';
 
 const ImageViewer = ({ isLoadingImage, image, loadImage }) => {
   useEffect(() => {
     loadImage();
-  }, []);
+  }, [loadImage]);
+
+  useInterval(() => {
+    loadImage();
+  }, 5000);
 
   return (
     <div className={styles.imageViewer}>
